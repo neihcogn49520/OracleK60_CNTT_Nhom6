8b7f2cf8462b1c04015519a71b4132e240599036
Trinh Trinh <6051071128@st.utc2.edu.vn>
2022-09-28 09:38:28 +0700

    save index and pay page

--- js/main.js
+++ js/main.js
@@ -78,4 +78,25 @@
 })(jQuery);
 /// 
 
+$(document).ready(function() {
+ 
+    $('.method').on('click', function() {
+      $('.method').removeClass('blue-border');
+      $(this).addClass('blue-border');
+    });
+   
+  })
+  var $cardInput = $('.input-fields input');
+   
+  $('.next-btn').on('click', function(e) {
+   
+    $cardInput.removeClass('warning');
+   
+    $cardInput.each(function() {    
+       var $this = $(this);
+       if (!$this.val()) {
+         $this.addClass('warning');
+       }
+    })
+  });
 
